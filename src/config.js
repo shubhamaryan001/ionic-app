3ada1483a20f62261e7104865a059fb34539f018
Shubham <saurabharyan30@gmail.com>
2019-11-19 14:05:11 +0530

    first

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:8000/api";
+export const API = "https://nodelatest-api.herokuapp.com/api";
 
 const Home_API = "http://localhost:8000/api";
 const External_API = "https://nodelatest-api.herokuapp.com/api";
