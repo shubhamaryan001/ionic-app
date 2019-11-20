53d3b067840149fa22cc968a80e0da949b2076f2
Shubham <saurabharyan30@gmail.com>
2019-11-20 14:04:41 +0530

    first

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API = "https://nodelatest-api.herokuapp.com/api";
-export const Front_API = "http://localhost:3001";
+export const Front_API = "https://ionic-app.now.sh";
 
 const Home_API = "http://localhost:8000/api";
 const External_API = "https://nodelatest-api.herokuapp.com/api";
